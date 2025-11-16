21e30708c214cf595354bb60df68a40adcf7a514
salma <s.belaalia4677@uca.ac.ma>
2025-11-17 00:36:07 +0100

    function 2 & 3 & 4 & 5

--- main.js
+++ main.js
@@ -6,3 +6,26 @@ function getFirstEmployeeFirstName(employees) {
 }
 console.log(getFirstEmployeeFirstName(employees));
 
+// function 2:
+function getLastEmployeeLastName(employees) {
+    return employees[employees.length - 1].lastName;
+}
+console.log(getLastEmployeeLastName(employees));
+
+// function 3:
+function getEmployeeDepartmentById(employees, id) {
+    for (let i=0; i<employees.length; i++) {
+        if (employees[i].id === id) 
+            return employees[i].department;
+    }
+}
+console.log(getEmployeeDepartmentById(employees, 5));
+
+// function 4:
+function getEmployeeDepartmentById(employees, id) {
+    for (let i=0; i<employees.length; i++) {
+        if (employees[i].id === id) 
+            return employees[i].department;
+    }
+}
+console.log(getEmployeeDepartmentById(employees, 5));
