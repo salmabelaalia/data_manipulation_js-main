282b7e4b1fe6caa324cf628f32b0f4e8d24b7ff3
salma <s.belaalia4677@uca.ac.ma>
2025-11-17 00:13:49 +0100

    function 1

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 /* ==== Easy - Basic Data Retrieval ==== */
 
 // function 1:
-function get
+function getFirstEmployeeFirstName(employees) {
+    return employees[0].firstName;
+}
+console.log(getFirstEmployeeFirstName(employees));
+
